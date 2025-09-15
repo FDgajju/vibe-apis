faf65886c55e13fed4e1431592c73a78134dcf23
Gajendra sahu <92583148+FDgajju@users.noreply.github.com>
2025-09-15 13:38:59 +0530

    add: security headers, xss check and rate limit

--- src/middlewares/injectMiddlewares.ts
+++ src/middlewares/injectMiddlewares.ts
@@ -1,11 +1,14 @@
 import cors from "cors";
 import express, { type Application } from "express";
 import { CORS_ALLOWED_ORIGINS } from "../constants";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import rateLimit from "express-rate-limit";
 
 /*
     add allowed origins options in your env separated by comma
     eg. CORS_ALLOWED_ORIGINS=http://localhost:3000,https://example.com
-    */
+*/
 
 export const injectMiddleware = (app: Application) => {
   app.use(
@@ -16,4 +19,14 @@ export const injectMiddleware = (app: Application) => {
   );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(helmet());
+  app.use(mongoSanitize());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 min
+      max: 100,
+      standardHeaders: true,
+      legacyHeaders: false,
+    })
+  );
 };
